26cff539448acb461b1396a2fd65d36e6ebae517
Kayla D. Coleman <kdcolem2@ncsu.edu>
2020-01-05 00:37:47 -0500

    Designed and created home page

---
+++ animatedNavBar/navtoggle.js
@@ -0,0 +1,9 @@
+/* Javascript Code for Animated Navigation Bar
+Author: Kayla D. Coleman
+Author URI: github.com/kdcoleman
+*/
+
+// Animation to open navigation bar
+
+
+// Animation to collapse navigation bar
