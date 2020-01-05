d40150a5ad6cd12d899df3ab9ce74f440e00d108
Kayla D. Coleman <kdcolem2@ncsu.edu>
2020-01-05 17:32:55 -0500

    Implement nav bar toggle using active class; Change nav toggle icons to custom div elements

--- animatedNavBar/navtoggle.js
+++ animatedNavBar/navtoggle.js
@@ -3,7 +3,15 @@ Author: Kayla D. Coleman
 Author URI: github.com/kdcoleman
 */
 
-// Animation to open navigation bar
+// Change all classes to active to toggle navigation bar
+function toggleNavBar() {
+  elems = document.getElementsByClassName('activate');
 
+  for (var i = 0; i < elems.length; i++) {
+    elems[i].classList.toggle('active');
+  }
+}
 
-// Animation to collapse navigation bar
+// Create event listener for "onclick" of nav-toggle button
+var navToggle = document.getElementById('nav-toggle');
+navToggle.addEventListener("click", toggleNavBar);
