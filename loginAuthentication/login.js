81ae7e7a52ecac23c107be51fc6a3b189c0df13e
Kayla D. Coleman <kdcolem2@ncsu.edu>
2020-01-06 18:37:18 -0500

    Add methods to show login alert on click of login link and close login alert on click of close button

--- loginAuthentication/login.js
+++ loginAuthentication/login.js
@@ -3,6 +3,16 @@ Author: Kayla D. Coleman
 Author URI: github.com/kdcoleman
 */
 
+// Show login alert onclick of login link
+document.getElementById('loginLink').addEventListener('click', function(){
+  document.getElementById('loginAlert').classList.toggle('active');
+})
+
+// Close login alert onclick of close button
+document.getElementById('closeButton').addEventListener('click', function(){
+  document.getElementById('loginAlert').classList.toggle('active');
+})
+
 // Methods to check email and password
 function checkEmail() {
 
@@ -12,7 +22,7 @@ function checkPassword() {
 
 }
 
-// Check credentials onclick of login button
+// Check credentials onclick of login button on alert
 document.getElementById('loginButton').addEventListener('click', function(){
   checkEmail();
   checkPassword();
