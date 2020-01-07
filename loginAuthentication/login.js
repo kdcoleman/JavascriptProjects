e869a8c16da941bd4323304f3ea762d189b27b6a
Kayla D. Coleman <kdcolem2@ncsu.edu>
2020-01-07 13:19:48 -0500

    Validate email and password for login

--- loginAuthentication/login.js
+++ loginAuthentication/login.js
@@ -3,42 +3,93 @@ Author: Kayla D. Coleman
 Author URI: github.com/kdcoleman
 */
 
+var loginLink = document.getElementById('loginLink');
+var loginAlert = document.getElementById('loginAlert');
+var closeButton = document.getElementById('closeLogin');
+var emailMsg = document.getElementById('emailErrorMessage');
+var passwdMsg = document.getElementById('passwdErrorMessage');
+var loginForm = document.forms['loginForm'];
+var emailPattern = "[a-z0-9._%+-]+@[a-z0-9.-]+(\.[a-z]{2,3})+$";
+// "[^\s@]+@[^\s@]+\.[^\s@]{2,3}$";
+// "[a-z0-9._%+-]+@[a-z0-9.-]+(\.[a-z]{2,3})+$";
+
+// Set email pattern
+loginForm.elements.emailAddress.pattern = emailPattern;
+
 // Show login alert onclick of login link
-document.getElementById('loginLink').addEventListener('click', function(){
-  document.getElementById('loginAlert').classList.toggle('active');
+loginLink.addEventListener('click', function(){
+  loginAlert.classList.toggle('active');
 })
 
 // Close login alert onclick of close button
-document.getElementById('closeButton').addEventListener('click', function(){
-  document.getElementById('loginAlert').classList.toggle('active');
+closeButton.addEventListener('click', function(){
+  loginAlert.classList.toggle('active');
+  loginForm.reset();
+  emailMsg.innerHTML = "";
+  passwdMsg.innerHTML = "";
 })
 
-// Methods to check email and password
-function checkEmail() {
+// Methods to validate email and password
+function validateEmail() {
+  var input =  document.getElementById('emailAddress');
+  var validityState = input.validity;
+
+  if (validityState.valueMissing) {
+    input.setCustomValidity('Required');
+    emailMsg.innerHTML  = "Required";
+  }
+  else if (validityState.typeMismatch) {
+    input.setCustomValidity('Email address is invalid');
+    emailMsg.innerHTML  = "Email address is invalid";
+    loginForm.elements.emailAddress.value = "";
+  }
+  else if (validityState.patternMismatch) {
+    input.setCustomValidity('Email address is invalid');
+    emailMsg.innerHTML  = "Email address is invalid";
+    loginForm.elements.emailAddress.value = "";
+
+  }
 
 }
 
-function checkPassword() {
+function validatePassword() {
+  var input =  document.getElementById('passwd');
+  var validityState = input.validity;
 
-}
-
-// Check credentials onclick of login button on alert
-document.getElementById('loginButton').addEventListener('click', function(){
-  checkEmail();
-  checkPassword();
-})
-
-// Check credentials when press enter in input fields
-document.getElementById('email').onkeypress = function (event) {
-  if (event.keyCode == 13) {
-    checkEmail();
-    checkPassword();
+  if (validityState.valueMissing) {
+    input.setCustomValidity('Required');
+    passwdMsg.innerHTML = "Required";
+  }
+  else if (validityState.tooShort) {
+    input.setCustomValidity('Password must be at least 8 characters');
+    passwdMsg.innerHTML = "Password must be at least 8 characters";
+    loginForm.elements.passwd.value = "";
   }
 }
 
-document.getElementById('password').onkeypress = function (event) {
+// Validate credentials onclick of login button on alert
+document.getElementById('loginButton').addEventListener('click', function(){
+  emailMsg.innerHTML = "";
+  passwdMsg.innerHTML = "";
+  validateEmail();
+  validatePassword();
+})
+
+// Validate credentials when press enter in input fields
+document.getElementById('emailAddress').onkeypress = function (event) {
   if (event.keyCode == 13) {
-    checkEmail();
-    checkPassword();
+    emailMsg.innerHTML = "";
+    passwdMsg.innerHTML = "";
+    validateEmail();
+    validatePassword();
+  }
+}
+
+document.getElementById('passwd').onkeypress = function (event) {
+  if (event.keyCode == 13) {
+    emailMsg.innerHTML = "";
+    passwdMsg.innerHTML = "";
+    validateEmail();
+    validatePassword();
   }
 }
