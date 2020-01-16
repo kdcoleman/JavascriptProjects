12e065e5ecc48739acdd114bd696b6ff51a60ed8
Kayla D. Coleman <kdcolem2@ncsu.edu>
2020-01-16 17:04:37 -0500

    Start Django login app; Create User model

---
+++ kaysworld/login/models.py
@@ -0,0 +1,21 @@
+import datetime
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    join_date = models.DateTimeField('date joined')
+
+    def joined_recently(self):
+        return timezone.now() - datetime.timedelta(days=7) <= self.join_date <= timezone.now()
+
+    def __str__(self):
+        first = self.first_name
+        last = self.last_name
+        full = first + " " + last
+        return full
