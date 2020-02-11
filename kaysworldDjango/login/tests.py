c86faa1396bef52fd55b4192c9ce182766dff93c
Kayla D. Coleman <kdcolem2@ncsu.edu>
2020-02-11 01:55:37 -0500

    Add tests for signup, login, and logout views

--- kaysworldDjango/login/tests.py
+++ kaysworldDjango/login/tests.py
@@ -1,15 +1,35 @@
 from django.test import TestCase
+from django.urls import reverse
 from .models import User
 from .forms import LoginForm, SignupForm
-from django.contrib.auth import authenticate
-from django.urls import reverse
 
 # Create your tests here.
 
-class UserFormTests(TestCase):
+class UserSignupFormTests(TestCase):
+    def test_blank_signup_form(self):
+        """
+        Signup form with no data
+        """
+        data = {'first_name': '',
+                'last_name': '',
+                'email': '',
+                'password1': '',
+                'password2': ''
+        }
+
+        form = SignupForm(data)
+
+        self.assertFalse(form.is_valid())
+        self.assertEquals(form.errors['first_name'], ['This field is required.'])
+        self.assertEquals(form.errors['last_name'], ['This field is required.'])
+        self.assertEquals(form.errors['email'], ['This field is required.'])
+        self.assertEquals(form.errors['password1'], ['This field is required.'])
+        self.assertEquals(form.errors['password2'], ['This field is required.'])
+
+
     def test_signup_form_with_valid_new_user_data(self):
         """
-        Sign up new user with valid data
+        The signup form is valid when valid data is provided.
         """
         data = {'first_name': 'First',
                 'last_name': 'Last',
@@ -19,12 +39,14 @@ class UserFormTests(TestCase):
         }
 
         form = SignupForm(data)
+
         self.assertTrue(form.is_valid())
 
 
     def test_signup_form_with_different_passwords(self):
         """
-        Sign up new user where password1 and password2 do not match
+        The signup form returns an email field error when password1 and password2
+        do not match.
         """
         data = {'first_name': 'First',
                 'last_name': 'Last',
@@ -34,13 +56,15 @@ class UserFormTests(TestCase):
         }
 
         form = SignupForm(data)
+
         self.assertFalse(form.is_valid())
         self.assertEquals(form.errors['password2'], ['The two password fields didn’t match.'])
 
 
     def test_signup_form_with_taken_email(self):
         """
-        Sign up new user with an email that's already in use
+        The signup form returns an email field error when an email that's already in use
+        is provided.
         """
         user = User.objects.create_user(first_name='Jane', last_name='Doe',
                     email='janedoe@example.com', password='Testing1!')
@@ -53,13 +77,30 @@ class UserFormTests(TestCase):
         }
 
         form = SignupForm(data)
+
         self.assertFalse(form.is_valid())
         self.assertEquals(form.errors['email'], ['Sorry, this email is already in use.'])
 
 
+class UserLoginFormTests(TestCase):
+    def test_blank_login_form(self):
+        """
+        The login form in invalid when the fields are blank.
+        """
+        data = {'username': '',
+                'password': ''
+        }
+
+        form = LoginForm(data=data)
+
+        self.assertFalse(form.is_valid())
+        self.assertEquals(form.errors['username'], ['This field is required.'])
+        self.assertEquals(form.errors['password'], ['This field is required.'])
+
+
     def test_login_form_with_valid_credentials(self):
         """
-        Login a user with valid credentials
+        The login form is valid when valid credentials are provided.
         """
         user = User.objects.create_user(first_name='Jane', last_name='Doe',
                     email='janedoe@example.com', password='Testing1!')
@@ -69,12 +110,13 @@ class UserFormTests(TestCase):
         }
 
         form = LoginForm(data=data)
+
         self.assertTrue(form.is_valid())
 
 
     def test_login_form_with_incorrect_email(self):
         """
-        Login a user with the incorrect email
+        Form returns a email field error when the incorrect email is provided.
         """
         user = User.objects.create_user(first_name='Jane', last_name='Doe',
                     email='janedoe@example.com', password='Testing1!')
@@ -84,13 +126,15 @@ class UserFormTests(TestCase):
         }
 
         form = LoginForm(data=data)
+
         self.assertFalse(form.is_valid())
         self.assertEquals(form.errors['username'], ['Account not found. Verify the email is correct.'])
 
 
     def test_login_form_with_incorrect_password(self):
         """
-        Login a user with the incorrect password
+        Form returns a password field error when the incorrect password
+        is provided.
         """
         user = User.objects.create_user(first_name='Jane', last_name='Doe',
                     email='janedoe@example.com', password='Testing1!')
@@ -100,5 +144,149 @@ class UserFormTests(TestCase):
         }
 
         form = LoginForm(data=data)
+
         self.assertFalse(form.is_valid())
         self.assertEquals(form.errors['password'], ['Incorrect password.'])
+
+
+class UserLoginViewTests(TestCase):
+    def test_login_view_with_valid_credentials(self):
+        """
+        Login view redirects to home view with valid credentials.
+        """
+        user = User.objects.create_user(first_name='Jane', last_name='Doe',
+                    email='janedoe@example.com', password='Testing1!')
+
+        data = {'username': 'janedoe@example.com',
+                'password': 'Testing1!'
+        }
+
+        response = self.client.post('/login/login/', data, follow=True)
+
+        self.assertRedirects(response, reverse('login:home', args=(user.id,)), status_code=302, target_status_code=200)
+        self.assertEqual(len(response.redirect_chain), 1)
+
+
+    def test_login_view_with_incorrect_email(self):
+        """
+        When the incorrect email is entered on login form, the form is displayed
+        again in the login view with the email field form error.
+        """
+        user = User.objects.create_user(first_name='Jane', last_name='Doe',
+                    email='janedoe@example.com', password='Testing1!')
+
+        data = {'username': 'janedoe2@example.com',
+                'password': 'Testing1!'
+        }
+
+        response = self.client.post('/login/login/', data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Account not found. Verify the email is correct.')
+
+
+    def test_login_view_with_incorrect_password(self):
+        """
+        When the incorrect password is entered on login form, the form is displayed
+        again in the login view with the password field form error.
+        """
+        user = User.objects.create_user(first_name='Jane', last_name='Doe',
+                    email='janedoe@example.com', password='Testing1!')
+
+        data = {'username': 'janedoe@example.com',
+                'password': 'Testing2!'
+        }
+
+        response = self.client.post('/login/login/', data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Incorrect password.')
+
+
+class UserSignupViewTests(TestCase):
+    def test_signup_view_with_valid_credentials(self):
+        """
+        Signup view redirects to home view with valid credentials.
+        """
+        data = {'first_name': 'First',
+                'last_name': 'Last',
+                'email': 'email@example.com',
+                'password1': 'Testing!',
+                'password2': 'Testing!'
+        }
+
+        response = self.client.post('/login/signup/', data, follow=True)
+        user = User.objects.get(email='email@example.com')
+
+        self.assertRedirects(response, reverse('login:home', args=(user.id,)), status_code=302, target_status_code=200)
+        self.assertEqual(len(response.redirect_chain), 1)
+
+
+    def test_signup_view_with_taken_email(self):
+        """
+        When user enters an email that is already in use, the form is displayed
+        again in the signup view with the email field form error.
+        """
+        user = User.objects.create_user(first_name='Jane', last_name='Doe',
+                    email='janedoe@example.com', password='Testing1!')
+
+        data = {'first_name': 'First',
+                'last_name': 'Last',
+                'email': user.email,
+                'password1': 'Testing!',
+                'password2': 'Testing!'
+        }
+
+        response = self.client.post('/login/signup/', data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Sorry, this email is already in use.')
+
+
+    def test_signup_view_with_different_passwords(self):
+        """
+        When user enters a different confirmation password, the form is displayed
+        again in the signup view with the password field form error.
+        """
+        data = {'first_name': 'First',
+                'last_name': 'Last',
+                'email': 'email@example.com',
+                'password1': 'Testing!',
+                'password2': 'testing!'
+        }
+
+        response = self.client.post('/login/signup/', data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'The two password fields didn’t match.')
+
+
+class UserLogoutViewTests(TestCase):
+    def test_user_logout(self):
+        """
+        Logout view redirects to login view even when user is not logged in.
+        """
+        response = self.client.get('/login/logout/', follow=True)
+
+        self.assertRedirects(response, reverse('login:login'), status_code=302, target_status_code=200)
+        self.assertEqual(len(response.redirect_chain), 1)
+
+
+    def test_user_login_and_logout(self):
+        """
+        Login a user, then logut and redirect to the login view.
+        """
+
+        new_user = User.objects.create_user(first_name='Jane', last_name='Doe',
+                    email='janedoe@example.com', password='Testing1!')
+
+        data = {'username': 'janedoe@example.com',
+                'password': 'Testing1!'
+        }
+
+        self.client.post('/login/login/', data)
+
+        response = self.client.get('/login/logout/', follow=True)
+
+        self.assertRedirects(response, reverse('login:login'), status_code=302, target_status_code=200)
+        self.assertEqual(len(response.redirect_chain), 1)
